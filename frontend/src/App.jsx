1a787116c73eb4933845ad65882a0addba42a8f8
Thanh0201 <thanhho@metropolia.fi>
2025-12-03 10:37:10 +0200

    Part2: User Authenticate, frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { useState } from "react";
 // pages & components
 import Home from "./pages/HomePage";
 import AddJobPage from "./pages/AddJobPage";
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,37 @@
-const Navbar = () => {
+import { Link, useNavigate } from "react-router-dom";
+
+const Navbar = ({ isAuthenticated, setIsAuthenticated }) => {
+  const navigation = useNavigate();
+  
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+    localStorage.removeItem("user");
+    navigation("/");
+  }
+
   return (
     <nav className="navbar">
       <h1>Job Search</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/add-job">Add Job</a>
+        {!isAuthenticated && (
+          <>
+            <Link to={'/signup'}>Signup</Link>
+            <Link to={'/login'}>Login</Link>
+          </>
+        )}
+        {isAuthenticated && (
+          <>
+            <a href="/add-job">Add Job</a>
+            <a>
+              Welcome
+            </a>
+            <button onClick={handleLogout}>Logout</button>
+          </>
+        )}
       </div>
     </nav>
   );
 }
- 
+
 export default Navbar;
