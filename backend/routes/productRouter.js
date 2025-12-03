2859c52b24c1106fb9cce49d5ec76b8e28b268d1
Ngoc Nguyen <ngoc.nguyen15@metropolia.fi>
2025-12-03 11:29:35 +0200

    Phase 3: Implementing Protected Routes - Backend

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -6,12 +6,15 @@ const {
   updateProduct,
   deleteProduct,
 } = require("../controllers/productControllers");
-
+const requireAuth = require("../middleware/requireAuth");
 const router = express.Router();
 
 router.get("/", getAllProducts);
-router.post("/", createProduct);
 router.get("/:productId", getProductById);
+
+router.use(requireAuth);
+
+router.post("/", createProduct);
 router.put("/:productId", updateProduct);
 router.delete("/:productId", deleteProduct);
 
